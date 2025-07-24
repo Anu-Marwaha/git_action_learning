50f3f23dbb6af0ba083ca2f2bc1362cabbac5cd8
Anu <anumarwaha2010@gmail.com>
2025-07-24 18:33:13 -0400

    adding action 2

--- test.js
+++ test.js
@@ -1,7 +1,7 @@
 const greet = require('./index');
 
 if (greet('World') === 'Hello, World!') {
-  console.log('✅ Test passed!');
+  console.log('✅ Test passed! by Anu ');
   process.exit(0);
 } else {
   console.error('❌ Test failed!');
